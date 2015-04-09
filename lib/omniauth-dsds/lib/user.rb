99427bda7b848a08317d476834f3abd9a82851e3
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-09 14:48:18 +0100

    Populate the User model with email address and name as well as UID

--- lib/omniauth-dsds/lib/user.rb
+++ lib/omniauth-dsds/lib/user.rb
@@ -5,12 +5,16 @@ module Omniauth
 
       def self.build_from(auth_hash)
         new(
-          auth_hash.fetch("user").fetch("id")
+          uid:   auth_hash.fetch("user").fetch("uid"),
+          name:  auth_hash.fetch("profile").fetch("name"),
+          email: auth_hash.fetch("profile").fetch("email")
         )
       end
 
-      def initialize(uid)
-        @uid = uid
+      def initialize(uid:, name:, email:)
+        @uid   = uid
+        @name  = name
+        @email = email
       end
     end
   end
