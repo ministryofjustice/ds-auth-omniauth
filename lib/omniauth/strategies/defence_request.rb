a99923595e397dea69ba8398c9a5162b6a547dc0
Chris Carter <chris.carter@unboxedconsulting.com>
2015-04-09 15:17:37 +0100

    Remove unnecessary fields from initial Omniauth response * Extra data is provided by the /api/v1/me endpoint

--- lib/omniauth/strategies/defence_request.rb
+++ lib/omniauth/strategies/defence_request.rb
@@ -10,39 +10,10 @@ module OmniAuth
         redirect_url: ENV.fetch('AUTHENTICATION_REDIRECT_URI')
       }
 
-      uid { user_info["id"] }
-
-      info do
-        {
-          username: user_info["username"],
-          email: user_info["email"],
-          profile: profile,
-          roles: roles,
-        }
-      end
-
-      extra do
-        {
-          raw_info: raw_info,
-        }
-      end
+      uid { raw_info["user"]["uid"] }
 
       def raw_info
         @_raw_info ||= MultiJson.decode access_token.get("/api/v1/me").body
-      end
-
-      private
-
-      def user_info
-        raw_info["user"]
-      end
-
-      def profile
-        raw_info["profile"]
-      end
-
-      def roles
-        raw_info["roles"]
       end
     end
   end
