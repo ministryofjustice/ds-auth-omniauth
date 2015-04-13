f3e0a7cc9c54ed416dafb6110b2f580056516167
ansonK <ansonkelly@gmail.com>
2015-04-13 18:14:14 +0100

    Override callback_phase in strategy to check roles

--- lib/omniauth/strategies/defence_request.rb
+++ lib/omniauth/strategies/defence_request.rb
@@ -2,12 +2,28 @@ require "omniauth-oauth2"
 
 module OmniAuth
   module Strategies
+
+    module CallbackOverride
+      def callback_phase
+        if raw_info['roles'].nil? || raw_info['roles'].empty?
+          fail!(:unauthorized, OAuth2::CallbackError.new(:unauthorized, 'Unauthorized access'))
+        else
+          super
+        end
+      end
+    end
+
+    # Need to check roles after the access_token has been built by OmniAuth::OAuth2#callback_phase 
+    # but before the parent app is called by OmniAuth::Strategy#callback_phase
+    # so we place a module in the ancestors chain between them
+    OAuth2.send :include, CallbackOverride
+
     class DefenceRequest < OmniAuth::Strategies::OAuth2
       option :name, "defence_request"
 
       option :client_options, {
-        site: ENV.fetch('AUTHENTICATION_SITE_URL'),
-        redirect_url: ENV.fetch('AUTHENTICATION_REDIRECT_URI')
+        site: ENV.fetch('AUTHENTICATION_SITE_URL', nil),
+        redirect_url: ENV.fetch('AUTHENTICATION_REDIRECT_URI', nil)
       }
 
       uid { raw_info["profile"]["uid"] }
