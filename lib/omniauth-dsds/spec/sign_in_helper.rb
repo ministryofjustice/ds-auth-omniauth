67581d2a74dcb6486217f31dd6a8a87f16a7b1dd
Rob McKinnon <rob.mckinnon@digital.justice.gov.uk>
2015-04-24 13:32:26 +0100

    Set address via options, with corrected address fields.

--- lib/omniauth-dsds/spec/sign_in_helper.rb
+++ lib/omniauth-dsds/spec/sign_in_helper.rb
@@ -18,22 +18,20 @@ module Omniauth
         def mock_profile(options: {}, authentication_site_url: "http://app.example.com")
           response_body = {
             "user" => {
-                "email" => "bob.smith@world.com",
+              "email" => "bob.smith@world.com",
             },
             "profile" => {
-                "name" => options.fetch(:name) { "Bob Smith" },
-                "email" => options.fetch(:email) { "bob.smith@world.com" },
-                "telephone" => options.fetch(:telephone) { "0123456789" },
-                "mobile" => options.fetch(:mobile) { "071234567" },
-                "address" => {
-                    "line1" => "",
-                    "line2" => "",
-                    "city" => "",
-                    "postcode" => ""
-                },
-                "PIN" => "1234",
-                "organisation_uids" => options.fetch(:organisation_uids) { ["12345678-bcde-1234-abcd-1234567890","12345678-cdef-1234-abcd-1234567890"] },
-                "uid" =>  options.fetch(:uid) { "12345678-abcd-1234-abcd-1234567890" }
+              "name" => options.fetch(:name) { "Bob Smith" },
+              "email" => options.fetch(:email) { "bob.smith@world.com" },
+              "telephone" => options.fetch(:telephone) { "0123456789" },
+              "mobile" => options.fetch(:mobile) { "071234567" },
+              "address" => {
+                "full_address" => options.fetch(:full_address) { "" },
+                "postcode" => options.fetch(:postcode) { "" }
+              },
+              "PIN" => "1234",
+              "organisation_uids" => options.fetch(:organisation_uids) { ["12345678-bcde-1234-abcd-1234567890","12345678-cdef-1234-abcd-1234567890"] },
+              "uid" =>  options.fetch(:uid) { "12345678-abcd-1234-abcd-1234567890" }
             },
             "roles" => options.fetch(:roles) { ["admin", "foo", "bar"] }
           }.to_json
