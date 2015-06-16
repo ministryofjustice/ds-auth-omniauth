f0dbd18740aead935f2a06fe15da72b54a390087
Jakub Novotny <novotny.jakub@gmail.com>
2015-06-16 17:22:04 +0100

    Refactor the UserBuilder spec

--- spec/omniauth-dsds/lib/user_builder_spec.rb
+++ spec/omniauth-dsds/lib/user_builder_spec.rb
@@ -4,27 +4,10 @@ require_relative "../../../lib/omniauth-dsds/lib/user_builder"
 require "omniauth"
 
 RSpec.describe Omniauth::Dsds::UserBuilder, "#build_user" do
-  subject {
-    Omniauth::Dsds::UserBuilder.new(
-      app_id: app_id, app_secret: app_secret, token: token
-    )
-  }
+  let(:builder) { Omniauth::Dsds::UserBuilder.new(app_id: app_id, app_secret: app_secret, token: token) }
   let(:app_id) { "12345" }
   let(:app_secret) { "ABCDE" }
   let(:token) { "ZXCVB" }
-
-  context "with a blank token" do
-    let(:token) { nil }
-
-    it "returns nil" do
-      expect(subject.build_user).to be_nil
-    end
-  end
-
-  before do
-    allow(subject).to receive(:fetch_raw_info).with(token).and_return raw_info_response
-  end
-
   let(:roles) { ["cso"] }
   let(:raw_info_response) do
     {
@@ -40,20 +23,28 @@ RSpec.describe Omniauth::Dsds::UserBuilder, "#build_user" do
     }
   end
 
+  subject { builder.build_user }
+
+  before do
+    allow(builder).to receive(:fetch_raw_info).with(token).and_return raw_info_response
+  end
+
+  context "with a blank token" do
+    let(:token) { nil }
+
+    it { is_expected.to be nil }
+  end
+
   context "when raw_info has no roles for any of the organisations" do
     let(:roles) { [] }
 
-    it "returns nil" do
-      expect(subject.build_user).to be_nil
-    end
+    it { is_expected.to be nil }
   end
 
   context "when raw_info has no organisations" do
     let(:raw_info_response) { {"organisations" => [] } }
 
-    it "returns nil" do
-      expect(subject.build_user).to be_nil
-    end
+    it { is_expected.to be nil }
   end
 
   context "when raw_info has roles for at least one organisation" do
@@ -61,7 +52,7 @@ RSpec.describe Omniauth::Dsds::UserBuilder, "#build_user" do
       stub_user = double("User")
       expect(Omniauth::Dsds::User).to receive(:build_from).with(raw_info_response).and_return stub_user
 
-      expect(subject.build_user).to eq stub_user
+      is_expected.to eq stub_user
     end
   end
 end
