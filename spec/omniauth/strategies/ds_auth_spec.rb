5c2eb0802dadbdcb2fb80829da89305c5fb9bda0
ansonK <ansonkelly@gmail.com>
2015-07-14 14:45:37 +0100

    expose user info in omniauth.auth hash
    expose organisations a user belongs to in omniauth.auth hash
    
    remove explict role check from controller mixin - consuming apps can do themselves if needed
    
    expose user phone, mobile and address in User object

--- spec/omniauth/strategies/ds_auth_spec.rb
+++ spec/omniauth/strategies/ds_auth_spec.rb
@@ -35,6 +35,65 @@ describe "ds_auth strategy" do
     end
   end
 
+  describe "#uid" do
+    it "returns the users uid from the raw info" do
+      expect(subject).to receive(:raw_info).and_return({"user" => {"uid" => "abcdef"}})
+
+      expect(subject.uid).to eq("abcdef")
+    end
+  end
+
+  describe "#info" do
+    before do
+      json = File.read(File.expand_path(File.join(__FILE__, "../../../support/me.json"))).gsub('\\n','')
+      allow(subject).to receive(:fetch_raw_info).and_return json
+    end
+
+    it "has user name" do
+      expect(subject.info["name"]).to eq("Bob Smith")
+    end
+
+    it "has user email" do
+      expect(subject.info["email"]).to eq("bob@example.com")
+    end
+
+    it "has user phone" do
+      expect(subject.info["phone"]).to eq("01234567890")
+    end
+
+    it "has user mobile" do
+      expect(subject.info["mobile"]).to eq("07123456789")
+    end
+
+    it "has user full_address" do
+      expect(subject.info["full_address"]).to eq("1 Fake Street")
+    end
+
+    it "has user postcode" do
+      expect(subject.info["postcode"]).to eq("AB1 2CD")
+    end
+
+    it "has an array of organisations the user belongs to" do
+      expect(subject.info["organisations"]).to be_a Array
+    end
+
+    describe "organisations" do
+      let(:organisation) { subject.info["organisations"].first }
+
+      it "has a uid" do
+        expect(organisation["uid"]).to eq("b2e402e4-6e44-485b-87b8-b47343cfbedb")
+      end
+
+      it "has a name" do
+        expect(organisation["name"]).to eq("Bobs Company")
+      end
+
+      it "has roles the user has within that organisation" do
+        expect(organisation["roles"]).to eq(["admin", "viewer"])
+      end
+    end
+  end
+
   describe "raw_info" do
     before do
       allow(fake_access_token).to receive(:get).and_return fake_response
